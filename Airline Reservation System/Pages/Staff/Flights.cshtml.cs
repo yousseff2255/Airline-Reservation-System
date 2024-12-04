f86d97d5cab440d11c348badff407fa2a7dc4006
yousseff2255 <yousseff2255@gmail.com>
2024-12-04 12:29:40 +0200

    Add Flights Page for Staff

---
+++ Airline Reservation System/Pages/Staff/Flights.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Airline_Reservation_System.Pages.Staff
+{
+    public class FightsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
