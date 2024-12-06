d5ab9c33c2dd922db569b91eb4c8b23b813d6c3e
Ahmed Essam <ahmedessamfahim2005@gmail.com>
2024-12-06 16:39:49 +0200

    youssefHowaElsabab

---
+++ Airline Reservation System/Pages/Search.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebApplication4.Pages
+{
+    public class SearchModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
